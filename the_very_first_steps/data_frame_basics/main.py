3af722282117430cc328291bb87a6622baeecb20
sebastianEriksson87 <>
2026-02-02 11:36:07 +0000

    DataFrame Basics

---
+++ the_very_first_steps/data_frame_basics/main.py
@@ -0,0 +1,11 @@
+import pandas as pd 
+
+# Creating a dictionary
+animals_data = {'animal': ['Cat', 'Dog', 'Parrot', 'Cat', 'Parrot', 'Cat'],
+           'name': ['Dolly', 'Abbey', 'Erin', 'Kelly', 'Taffy', 'Odie']}
+
+# Write your code below
+animals = ___
+
+# Testing the result
+print(animals)
