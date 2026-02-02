47c49b8c7c82067ee309caa8b141716202f6b215
sebastianEriksson87 <>
2026-02-02 11:37:16 +0000

    DataFrame Basics

--- the_very_first_steps/data_frame_basics/main.py
+++ the_very_first_steps/data_frame_basics/main.py
@@ -5,7 +5,7 @@ animals_data = {'animal': ['Cat', 'Dog', 'Parrot', 'Cat', 'Parrot', 'Cat'],
            'name': ['Dolly', 'Abbey', 'Erin', 'Kelly', 'Taffy', 'Odie']}
 
 # Write your code below
-animals = ___
+animals = pd.DataFrame(animals_data)
 
 # Testing the result
 print(animals)
